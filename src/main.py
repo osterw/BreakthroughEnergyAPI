d97b535c0a02e890b93fd0b09d37dffa2bcf4580
Warren Oster <oster.warren@pm.me>
2025-05-13 11:03:55 -0600

    add pytz to requirements, implement API changes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
+from datetime import datetime
+import pytz
+from typing import Optional, Any, List
 
 app = FastAPI()
 
 
+def flatten(data: Any) -> List[Any]:
+    """Flatten a JSON entry into a list of keys and values"""
+    result = []
+    if isinstance(data, dict):
+        for key, value in data.items():
+            result.append(key)
+            result.extend(flatten(value))
+    elif isinstance(data, (list, tuple)):
+        for item in data:
+            result.extend(flatten(item))
+    else:
+        result.append(data)
+    return result
+
+
 @app.get("/helloworld")
-async def hello_world():
-    return "Hello World!"
+async def hello_world(
+    tz: Optional[str] = None,
+    accept: Optional[str] = Header(default="text/plain")
+):
+    message = "Hellow World!"
+    if tz:
+        try:
+            timezone = pytz.timezon(tz)
+            current_time = datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
+            message = f"{message} it is {current_time} in timezone: {tz}"
+        except pytz.exceptions.UnknownTimeZoneError:
+            raise HTTPException(status_code=400, detail=f"Your timezone {tz} is invalid")
+
+    if accept == "application/json":
+        return {"message": message}
+    return message
